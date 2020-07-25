8cf3673eda633b88c23be1c12db945d3fc4e5b0a
Aman Verma <amanverma1729@gmail.com>
2020-07-25 13:48:59 +0530

    Added data and assets

---
+++ data.js
@@ -0,0 +1,63 @@
+module.exports.PlaceData = [
+  {
+    title: "Venice",
+    img: require("./assets/venice.jpg"),
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    place: "Venice",
+    outerPlace: "Italy",
+  },
+  {
+    title: "Paris",
+    img: require("./assets/eiffel.jpg"),
+    desc:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    place: "Paris",
+    outerPlace: "France",
+  },
+  {
+    title: "Delhi",
+    img: require("./assets/indiagate.jpg"),
+    desc:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    place: "New Delhi",
+    outerPlace: "India",
+  },
+  {
+    title: "Italy",
+    img: require("./assets/venice.jpg"),
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    place: "Venice",
+    outerPlace: "Italy",
+  },
+  {
+    title: "Rome",
+    img: require("./assets/eiffel.jpg"),
+    desc:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    place: "Paris",
+    outerPlace: "France",
+  },
+];
+
+module.exports.HotelData = [
+  {
+    title: "Hotel Venice",
+    img: require("./assets/hotel1.jpg"),
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Hotel Paris",
+    img: require("./assets/hotel2.jpg"),
+    desc:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Hotel Delhi",
+    img: require("./assets/hotel3.jpg"),
+    desc:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
